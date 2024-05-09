680783bdd1ac6753cdc00884eb825d1f5474b4d8
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-05-09 09:25:50 +0600

    access data BY ID

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
-// Dummy data for posts
+// Dummy data 
 let posts = [
     { id: 1, text: 'Post 1', likes: 0, comments: [] },
     { id: 2, text: 'Post 2', likes: 0, comments: [] }
@@ -14,6 +14,18 @@ let posts = [
 app.get('/posts', (req, res) => {
     res.json(posts);
 });
+
+
+// Access a get data by ID 
+app.get('/posts/:id', (req, res) => {
+    const postId = parseInt(req.params.id);
+    const post = posts.find(post => post.id === postId);
+    if (!post) {
+      return res.status(404).json({ message: 'Post not found' });
+    }
+    res.json(post);
+  });
+  
 
 
 
