52707ca9b73988c443c411050e571b537ffb3db6
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-05-09 09:22:58 +0600

    Create a dammy data and get a all data api Create

--- index.js
+++ index.js
@@ -4,13 +4,22 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
+// Dummy data for posts
+let posts = [
+    { id: 1, text: 'Post 1', likes: 0, comments: [] },
+    { id: 2, text: 'Post 2', likes: 0, comments: [] }
+];
 
+// Get all posts
+app.get('/posts', (req, res) => {
+    res.json(posts);
+});
 
 
 
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
